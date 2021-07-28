2152856a897435106e6f7d572c8c84667a1ca7bc
Andrea Acevedo <andreaacevedo6@gmail.com>
2021-07-28 18:02:59 -0400

    index ready, new a la mitad

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # read
+  # Get all taks
+  get 'tasks', to: 'tasks#index', as: :tasks
+
+  # create
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
+
+  # edit
+  get 'tasks/:id/edit', to: 'tasks#edit'
+
+  # read
+  # Get one taks
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # update
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
